babdb13fc85905b5861f2daea0a86fe6b88fb622
Reda ZOUHAIRI <zouhairireda@gmail.com>
2017-02-12 21:37:53 +0000

    add other old pocs of oauth2 and angular/oauth2

---
+++ spring-security-pocs/spring-angularoauth/src/main/java/com/projects/spring_angularoauth/App.java
@@ -0,0 +1,13 @@
+package com.projects.spring_angularoauth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
